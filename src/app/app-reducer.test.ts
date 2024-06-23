9d750eda8e99c11feaffd36bc4db390bb51c4b98
SlavaMryasov <slavadevbox1@gmail.com>
2024-06-23 20:13:23 +0300

    without tasksReducer 2:48

---
+++ src/app/app-reducer.test.ts
@@ -0,0 +1,23 @@
+import { error } from 'console';
+import { appActions, AppInitialState, appReducer } from './appSlice'
+
+let startState: AppInitialState;
+
+beforeEach(() => {
+	startState = {
+		error: null,
+		status: 'idle',
+		isInitialized: false
+	}
+})
+
+test('correct error message should be set', () => {
+	const endState = appReducer(startState, appActions.setAppError({ error: 'some error' }))
+	expect(endState.error).toBe('some error');
+})
+
+test('correct status should be set', () => {
+	const endState = appReducer(startState, appActions.setAppStatus({ status: 'loading' }))
+	expect(endState.status).toBe('loading');
+})
+
